0f50b714bd8846d661580fa394f4e1ff7dc26beb
AlexCatalin21 <alexandru.g2101@gmail.com>
2021-07-21 15:16:17 +0300

    home page background and welcome message

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -6,9 +6,21 @@ import HomeCard from "./HomeCard.js";
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="container homePage">
       <div className="row homeContainer">
-        <div className="col">
+        <div className="col-sm-6">
+          <h1 className="welcomeMessage">Welcome!</h1>
+          <h3 className="homePageDescription">
+            Have you ever been in a situation where you or your group of
+            friends couldn't make a decision? Couldn't decide where you wanted
+            to travel or what food to order for the evening? Are you the manager
+            of a company and want to vote for employee of the month? On
+            VotingPlat you can make all these decisions and more by creating
+            voting campaigns and inviting only the people you want to vote in
+            the campaign you create.
+          </h3>
+        </div>
+        <div className="col-sm-3">
           <HomeCard
             cardTitle="Topic Campaign"
             img={topic}
@@ -16,8 +28,8 @@ export default function Home() {
             link="/add-topics-campaign"
           />
         </div>
-        <div className="col">
-        <HomeCard
+        <div className="col-sm-3">
+          <HomeCard
             cardTitle="Candidates Campaign"
             img={candidate}
             cardText="Here you can create a campaign where you can vote for a person from a list."
